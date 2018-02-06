16432f93ee027c82aaaa1c4521c30797155cb0bc
Paul Kehrer <pkehrer@vistaprint.com>
2018-02-05 21:00:57 -0500

    created pkehrer folder

---
+++ keyboards/satan/keymaps/pkehrer/keymap.c
@@ -0,0 +1,113 @@
+#include "satan.h"
+
+
+// Used for SHIFT_ESC
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BL 0
+#define _FL 1
+#define _AL 2
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: (Base Layer) Default Layer
+   * .-----------------------------------------------------------.
+   * |GESC | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |-----------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+   * |-----------------------------------------------------------|
+   * |CAPS-FN|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |-----------------------------------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN-A|Ctrl |
+   * '-----------------------------------------------------------'
+   */
+[_BL] = KEYMAP_ANSI(
+  KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
+  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
+  LT(_FL, KC_CAPS), \
+              KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, TG(_AL),KC_RCTL),
+
+  /* Keymap _FL: Function Layer
+   * .-----------------------------------------------------------.
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  DEL  |
+   * |-----------------------------------------------------------|
+   * |     |   |   |   |   |   |   |   |   |   |   |HOM|END|     |
+   * |-----------------------------------------------------------|
+   * |      |   |   |   |   |   |   |   |   |   |   |   |        |
+   * |-----------------------------------------------------------|
+   * |        |   |   |   |   |   |   |   |   |   |   |  ^       |
+   * |-----------------------------------------------------------|
+   * |    |    |    |                        |    |  <  | v |  > |
+   * '-----------------------------------------------------------'
+   */
+[_FL] = KEYMAP_ANSI(
+  _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_DEL,  \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME, KC_END,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_UP, \
+  _______,_______,_______,                _______,                        _______,KC_LEFT,KC_DOWN,KC_RIGHT),
+
+    /* Keymap _AL: Admin Layer
+   * .-----------------------------------------------------------.
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
+   * |-----------------------------------------------------------|
+   * |     |   |   |   |   |   |   |   |   |   |BRT|BL-|BL+|BL   |
+   * |-----------------------------------------------------------|
+   * |      |   |   |   |   |   |   |   |   |   |   |   |        |
+   * |-----------------------------------------------------------|
+   * |        |   |   |   |   |   |   |   |   |   |   |          |
+   * |-----------------------------------------------------------|
+   * |    |    |    |                        |    |    |    |    |
+   * '-----------------------------------------------------------'
+   */
+[_AL] = KEYMAP_ANSI(
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,     RESET,  \
+  XXXXXXX,   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,BL_BRTG,BL_DEC,BL_INC,BL_TOGG, \
+  XXXXXXX,      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, \
+  XXXXXXX,         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,      XXXXXXX, \
+  _______,   _______,_______,                XXXXXXX,                            _______,_______,_______,_______),
+};
+
+void breath(bool on, uint8_t period) {
+  if (on) {
+    if (!is_breathing()) {
+      breathing_enable();
+      breathing_period_set(period);
+    }
+  } else {
+    if (is_breathing()) {
+      breathing_period_set(period);
+      breathing_disable();
+    }
+  }
+}
+
+void matrix_init_user(void) {
+  breath(false, 3);
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  uint8_t layer = biton32(state);
+  switch (layer) {
+    case _BL:
+      breath(false, 3);
+      break;
+    case _FL:
+      breath(true, 3);
+      break;
+    default:
+      breath(true, 1);
+      break;
+  }
+  return state;
+}
--- keyboards/satan/satan.c
+++ keyboards/satan/satan.c
@@ -20,11 +20,11 @@ void led_init_ports(void) {
 }
 
 void led_set_kb(uint8_t usb_led) {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // Turn capslock on
-        PORTB &= ~(1<<2);
-    } else {
-        // Turn capslock off
-        PORTB |= (1<<2);
-    }
+    // if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+    //     // Turn capslock on
+    //     PORTB &= ~(1<<2);
+    // } else {
+    //     // Turn capslock off
+    //     PORTB |= (1<<2);
+    // }
 }
